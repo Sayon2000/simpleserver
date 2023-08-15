79d4572b9749de7de6097287b4c8cfdf7adc1224
Sayon Dutta <52020690+Sayon2000@users.noreply.github.com>
2023-08-15 21:28:46 +0530

    response based on request url

--- index.js
+++ index.js
@@ -4,9 +4,22 @@ const hostname = '127.0.0.1';
 const port = 4000;
 
 const server = http.createServer((req, res) => {
+
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Sayon Dutta');
+  res.setHeader('Content-Type', 'text/html');
+  let result = '<html>'+
+  '<head><title>Title</title>' ;
+  if(req.url === '/home')
+    result +=  '<body><h2>Welcome to home</h2></body>'+
+    '</html>'
+  else if(req.url === '/about')
+  result +=  '<body><h2>Welcome to About Us page</h2></body>'+
+  '</html>'
+  else if(req.url === '/node')
+  result +=  '<body><h2>Welcome to my Node Js project</h2></body>'+
+  '</html>'
+
+  res.end(result)
 });
 
 server.listen(port, hostname, () => {
